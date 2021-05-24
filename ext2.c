3de5f1e77d0866cd938dff0765f1908c450f52c3
javialbors <javier.albors@students.salle.url.edu>
2021-05-24 18:10:11 +0200

    Added EXT2 search functionality for phase 2

--- ext2.c
+++ ext2.c
@@ -1,5 +1,240 @@
 #include "ext2.h"
 
+void EXT2_find(int fd, ext_info info, char *filename) {
+	
+	int inode = 2;
+	int block_group = (inode-1) / info.inodes_per_group;
+	int inode_index = (inode-1) % info.inodes_per_group;
+	int group_base_offset = block_group * (info.blocks_per_group * info.block_size);
+	int bg_offset = group_base_offset + (SUPERBLOCK*2);
+	
+	lseek(fd, bg_offset + 8, SEEK_SET);
+	uint32_t bg_inode_table_block;
+	read(fd, &bg_inode_table_block, 4);
+	
+	int inode_table = group_base_offset + (bg_inode_table_block * info.block_size);
+	int inode_offset = inode_table + (inode_index * info.inode_size);
+	
+	uint32_t i_blocks;
+	lseek(fd, inode_offset + 28, SEEK_SET);
+	read(fd, &i_blocks, 4);
+
+	//Iterar segun i_blocks?
+	uint32_t data_block;
+	lseek(fd, inode_offset + 40 /*+ (i*4)*/, SEEK_SET);
+	read(fd, &data_block, 4);
+		
+	int inode_data_block = data_block * info.block_size;
+	uint16_t rec_len = 0;
+
+	do {
+		lseek(fd, group_base_offset + inode_data_block + rec_len, SEEK_SET);
+		uint32_t ind;
+		read(fd, &ind, 4);
+
+		lseek(fd, group_base_offset + inode_data_block + rec_len + 6, SEEK_SET);
+		uint8_t name_len;	
+		read(fd, &name_len, 1);
+
+		lseek(fd, group_base_offset + inode_data_block + rec_len + 7, SEEK_SET);
+		uint8_t filetype;
+		read(fd, &filetype, 1);
+
+		char *name = malloc(name_len + 1);
+		lseek(fd, group_base_offset + inode_data_block + rec_len + 8, SEEK_SET);
+		read(fd, name, name_len);
+		name[name_len] = '\0';
+		
+		if (filetype == EXT2_FT_FILE) {
+			if (!strcmp(name, filename)) {
+				char output[39];
+				sprintf(output, "File found. Occupies %d bytes\n", get_inode_size(fd, info, ind));
+				write(1, output, strlen(output));
+				return;
+			}
+		}
+
+		free(name);
+		
+		lseek(fd, group_base_offset + inode_data_block + rec_len + 4, SEEK_SET);
+		uint16_t rec;
+		read(fd, &rec, 2);
+		
+		rec_len += rec;
+	} while(rec_len < info.block_size);
+
+	write(1, "ERROR - File not found\n", strlen("ERROR - File not found\n"));
+}
+
+uint32_t get_inode_size(int fd, ext_info info, int inode) {
+	
+	int block_group = (inode-1) / info.inodes_per_group;
+	int inode_index = (inode-1) % info.inodes_per_group;
+	int group_base_offset = block_group * (info.blocks_per_group * info.block_size);
+	int bg_offset = group_base_offset + (SUPERBLOCK*2);
+	
+	lseek(fd, bg_offset + 8, SEEK_SET);
+	uint32_t bg_inode_table_block;
+	read(fd, &bg_inode_table_block, 4);
+	
+	int inode_table = group_base_offset + (bg_inode_table_block * info.block_size);
+	int inode_offset = inode_table + (inode_index * info.inode_size);
+
+	lseek(fd, inode_offset + 4, SEEK_SET);
+	uint32_t inode_size;
+	read(fd, &inode_size, 4);
+
+	return inode_size;
+}
+
+ext_info EXT2_info(int fd, int flag) {
+	
+	ext_info info;
+
+	if (lseek(fd, 0, SEEK_CUR) == -1) {
+		info.error = 1;
+		return info;
+	}
+	
+	uint16_t fs;
+	lseek(fd, SUPERBLOCK + 56, SEEK_SET);
+	read(fd, &fs, 2);
+
+	if (fs != MAGIC_NUMBER) {
+		info.error = 1;
+		return info;
+	}
+
+	info.error = 0;
+	strcpy(info.filesystem, "EXT2");
+
+	lseek(fd, SUPERBLOCK, SEEK_SET);
+	read(fd, &info.inodes_count, 4);
+	char inodes_count[11];
+	sprintf(inodes_count, "%d", info.inodes_count);
+
+	lseek(fd, SUPERBLOCK + 4, SEEK_SET);
+	read(fd, &info.blocks_count, 4);
+	char blocks_count[11];
+	sprintf(blocks_count, "%d", info.blocks_count);
+
+	lseek(fd, SUPERBLOCK + 8, SEEK_SET);
+	read(fd, &info.reserved_blocks, 4);
+	char reserved_blocks[11];
+	sprintf(reserved_blocks, "%d", info.reserved_blocks);
+
+	lseek(fd, SUPERBLOCK + 12, SEEK_SET);
+	read(fd, &info.free_blocks, 4);
+	char free_blocks[11];
+	sprintf(free_blocks, "%d", info.free_blocks);
+
+	lseek(fd, SUPERBLOCK + 16, SEEK_SET);
+	read(fd, &info.free_inodes, 4);
+	char free_inodes[11];
+	sprintf(free_inodes, "%d", info.free_inodes);
+
+	lseek(fd, SUPERBLOCK + 32, SEEK_SET);
+	read(fd, &info.blocks_per_group, 4);
+	char blocks_per_group[11];
+	sprintf(blocks_per_group, "%d", info.blocks_per_group);
+
+	lseek(fd, SUPERBLOCK + 36, SEEK_SET);
+	read(fd, &info.frags_per_group, 4);
+	char frags_per_group[11];
+	sprintf(frags_per_group, "%d", info.frags_per_group);
+
+	lseek(fd, SUPERBLOCK + 40, SEEK_SET);
+	read(fd, &info.inodes_per_group, 4);
+	char inodes_per_group[11];
+	sprintf(inodes_per_group, "%d", info.inodes_per_group);
+	
+	lseek(fd, SUPERBLOCK + 84, SEEK_SET);
+	read(fd, &info.first_inode, 4);
+	char first_inode[11];
+	sprintf(first_inode, "%d", info.first_inode);
+
+	lseek(fd, SUPERBLOCK + 88, SEEK_SET);
+	read(fd, &info.inode_size, 2);
+	char inode_size[6];
+	sprintf(inode_size, "%d", info.inode_size);
+
+	lseek(fd, SUPERBLOCK + 24, SEEK_SET);
+	read(fd, &info.block_size, 4);
+	info.block_size = 1024 << info.block_size;
+	char block_size[11];
+	sprintf(block_size, "%d", info.block_size);
+	
+	lseek(fd, SUPERBLOCK + 20, SEEK_SET);
+	read(fd, &info.first_block, 4);
+	char first_block[11];
+	sprintf(first_block, "%d", info.first_block);
+
+	lseek(fd, SUPERBLOCK + 120, SEEK_SET);
+	read(fd, info.volume_name, 16);
+//	volume_name[16] = '\0';
+	
+	lseek(fd, SUPERBLOCK + 44, SEEK_SET);
+	read(fd, &info.last_mount, 4);
+	char *last_mount = unixt_to_date(info.last_mount);
+
+	lseek(fd, SUPERBLOCK + 48, SEEK_SET);
+	read(fd, &info.last_write, 4);
+	char *last_write = unixt_to_date(info.last_write);
+	
+	lseek(fd, SUPERBLOCK + 64, SEEK_SET);
+	read(fd, &info.last_check, 4);
+	char *last_check = unixt_to_date(info.last_check);
+	
+	if (flag == EXT_SHOW) {
+		write(1, "\n------ Filesystem Information ------\n\n", strlen("\n------ Filesystem Information ------\n\n"));
+	
+		write(1, "Filesystem: ", strlen("Filesystem: "));
+		write(1, info.filesystem, strlen(info.filesystem));
+
+		write(1, "\n\nINODE INFO\n", strlen("\n\nINODE INFO\n"));
+		write(1, "Inode size: ", strlen("Inode size: "));
+		write(1, inode_size, strlen(inode_size));
+		write(1, "\nNumber of inodes: ", strlen("\nNumber of inodes: "));
+		write(1, inodes_count, strlen(inodes_count));
+		write(1, "\nFirst inode: ", strlen("\nFirst inode: "));
+		write(1, first_inode, strlen(first_inode));
+		write(1, "\nInodes per group: ", strlen("\nInodes per group: "));
+		write(1, inodes_per_group, strlen(inodes_per_group));
+		write(1, "\nFree inodes: ", strlen("\nFree inodes: "));
+		write(1, free_inodes, strlen(free_inodes));
+
+		write(1, "\n\nBLOCK INFO\n", strlen("\n\nBLOCK INFO\n"));
+		write(1, "Block size: ", strlen("Block size: "));
+		write(1, block_size, strlen(block_size));
+		write(1, "\nReserved blocks: ", strlen("\nReserved blocks: "));
+		write(1, reserved_blocks, strlen(reserved_blocks));
+		write(1, "\nFree blocks: ", strlen("\nFree blocks: "));
+		write(1, free_blocks, strlen(free_blocks));
+		write(1, "\nTotal blocks: ", strlen("\nTotal blocks: "));
+		write(1, blocks_count, strlen(blocks_count));
+		write(1, "\nFirst block: ", strlen("\nFirst block: "));
+		write(1, first_block, strlen(first_block));
+		write(1, "\nBlocks per group: ", strlen("\nBlocks per group: "));
+		write(1, blocks_per_group, strlen(blocks_per_group));
+		write(1, "\nFragments per group: ", strlen("\nFragments per group: "));
+		write(1, frags_per_group, strlen(frags_per_group));
+
+		write(1, "\n\nVOLUME INFO\n", strlen("\n\nVOLUME INFO\n"));
+		write(1, "Volume name: ", strlen("Volume name: "));
+		write(1, info.volume_name, strlen(info.volume_name));
+		write(1, "\nLast check: ", strlen("\nLast check: "));
+		write(1, last_check, strlen(last_check));
+		write(1, "\nLast mount: ", strlen("\nLast mount: "));
+		write(1, last_mount, strlen(last_mount));
+		write(1, "\nLast write: ", strlen("\nLast write: "));
+		write(1, last_write, strlen(last_write));
+
+		write(1, "\n\n", 2);
+	}
+
+	return info;
+}
+
 char * unixt_to_date(uint32_t time) {
 	time_t t = time;
 	struct tm *time_struct;
@@ -12,162 +247,4 @@ char * unixt_to_date(uint32_t time) {
 	strftime(timeformat, 100, "%c", time_struct);
 
 	return timeformat;
-}
-
-int EXT2_info(int fd, int flag) {
-	
-	if (lseek(fd, 0, SEEK_CUR) == -1) return 0;
-	
-	char filesystem[5];
-	char inodes_count[11];
-	char blocks_count[11];
-	char reserved_blocks[11];
-	char free_blocks[11];
-	char free_inodes[11];
-	char blocks_per_group[11];
-	char frags_per_group[11];
-	char inodes_per_group[11];
-	char first_inode[11];
-	char inode_size[6];
-	char block_size[11];
-	
-	char volume_name[17];
-	char *last_mount;
-	char *last_write;
-	char *last_check;
-
-	uint16_t fs;
-	lseek(fd, SUPERBLOCK + 56, SEEK_SET);
-	read(fd, &fs, 2);
-
-	if (fs != MAGIC_NUMBER) return 0;
-	
-	if (flag != EXT_SHOW) {
-		close(fd);
-		return 1;
-	}
-
-	strcpy(filesystem, "EXT2");
-
-	uint32_t inc;
-	lseek(fd, SUPERBLOCK, SEEK_SET);
-	read(fd, &inc, 4);
-	sprintf(inodes_count, "%d", inc);
-
-	uint32_t bc;
-	lseek(fd, SUPERBLOCK + 4, SEEK_SET);
-	read(fd, &bc, 4);
-	sprintf(blocks_count, "%d", bc);
-
-	uint32_t rb;
-	lseek(fd, SUPERBLOCK + 8, SEEK_SET);
-	read(fd, &rb, 4);
-	sprintf(reserved_blocks, "%d", rb);
-
-	uint32_t fb;
-	lseek(fd, SUPERBLOCK + 12, SEEK_SET);
-	read(fd, &fb, 4);
-	sprintf(free_blocks, "%d", fb);
-
-	uint32_t fi;
-	lseek(fd, SUPERBLOCK + 16, SEEK_SET);
-	read(fd, &fi, 4);
-	sprintf(free_inodes, "%d", fi);
-
-	uint32_t bpg;
-	lseek(fd, SUPERBLOCK + 32, SEEK_SET);
-	read(fd, &bpg, 4);
-	sprintf(blocks_per_group, "%d", bpg);
-
-	uint32_t fpg;
-	lseek(fd, SUPERBLOCK + 36, SEEK_SET);
-	read(fd, &fpg, 4);
-	sprintf(frags_per_group, "%d", fpg);
-
-	uint32_t ipg;
-	lseek(fd, SUPERBLOCK + 40, SEEK_SET);
-	read(fd, &ipg, 4);
-	sprintf(inodes_per_group, "%d", ipg);
-	
-	uint32_t fin;
-	lseek(fd, SUPERBLOCK + 84, SEEK_SET);
-	read(fd, &fin, 4);
-	sprintf(first_inode, "%d", fin);
-
-	uint16_t is;
-	lseek(fd, SUPERBLOCK + 88, SEEK_SET);
-	read(fd, &is, 2);
-	sprintf(inode_size, "%d", is);
-
-	uint32_t bs;
-	lseek(fd, SUPERBLOCK + 24, SEEK_SET);
-	read(fd, &bs, 4);
-	bs = 1024 << bs;
-	sprintf(block_size, "%d", bs);
-
-	lseek(fd, SUPERBLOCK + 120, SEEK_SET);
-	read(fd, volume_name, 16);
-//	volume_name[16] = '\0';
-	
-	uint32_t lm;
-	lseek(fd, SUPERBLOCK + 44, SEEK_SET);
-	read(fd, &lm, 4);
-	last_mount = unixt_to_date(lm);
-
-	uint32_t lw;
-	lseek(fd, SUPERBLOCK + 48, SEEK_SET);
-	read(fd, &lw, 4);
-	last_write = unixt_to_date(lw);
-	
-	uint32_t lc;
-	lseek(fd, SUPERBLOCK + 64, SEEK_SET);
-	read(fd, &lc, 4);
-	last_check = unixt_to_date(lc);
-	
-	write(1, "\n------ Filesystem Information ------\n\n", strlen("\n------ Filesystem Information ------\n\n"));
-	
-	write(1, "Filesystem: ", strlen("Filesystem: "));
-	write(1, filesystem, strlen(filesystem));
-
-	write(1, "\n\nINODE INFO\n", strlen("\n\nINODE INFO\n"));
-	write(1, "Inode size: ", strlen("Inode size: "));
-	write(1, inode_size, strlen(inode_size));
-	write(1, "\nNumber of inodes: ", strlen("\nNumber of inodes: "));
-	write(1, inodes_count, strlen(inodes_count));
-	write(1, "\nFirst inode: ", strlen("\nFirst inode: "));
-	write(1, first_inode, strlen(first_inode));
-	write(1, "\nFree inodes: ", strlen("\nFree inodes: "));
-	write(1, free_inodes, strlen(free_inodes));
-	write(1, "\nInodes per group: ", strlen("\nInodes per group: "));
-	write(1, inodes_per_group, strlen(inodes_per_group));
-
-	write(1, "\n\nBLOCK INFO\n", strlen("\n\nBLOCK INFO\n"));
-	write(1, "Block size: ", strlen("Block size: "));
-	write(1, block_size, strlen(block_size));
-	write(1, "\nReserved blocks: ", strlen("\nReserved blocks: "));
-	write(1, reserved_blocks, strlen(reserved_blocks));
-	write(1, "\nFree blocks: ", strlen("\nFree blocks: "));
-	write(1, free_blocks, strlen(free_blocks));
-	write(1, "\nTotal blocks: ", strlen("\nTotal blocks: "));
-	write(1, blocks_count, strlen(blocks_count));
-	write(1, "\nBlocks per group: ", strlen("\nBlocks per group: "));
-	write(1, blocks_per_group, strlen(blocks_per_group));
-	write(1, "\nFragments per group: ", strlen("\nFragments per group: "));
-	write(1, frags_per_group, strlen(frags_per_group));
-
-	write(1, "\n\nVOLUME INFO\n", strlen("\n\nVOLUME INFO\n"));
-	write(1, "Volume name: ", strlen("Volume name: "));
-	write(1, volume_name, strlen(volume_name));
-	write(1, "\nLast check: ", strlen("\nLast check: "));
-	write(1, last_check, strlen(last_check));
-	write(1, "\nLast mount: ", strlen("\nLast mount: "));
-	write(1, last_mount, strlen(last_mount));
-	write(1, "\nLast write: ", strlen("\nLast write: "));
-	write(1, last_write, strlen(last_write));
-
-	write(1, "\n\n", 2);
-
-	close(fd);
-
-	return 1;
 }
--- ext2.h
+++ ext2.h
@@ -12,11 +12,38 @@
 #define SUPERBLOCK 1024
 #define MAGIC_NUMBER 0xEF53
 
+#define EXT2_FT_FILE 1
+#define EXT2_FT_DIR 2
+
 enum {
 	EXT_SHOW = 1,
 	EXT_CHECK = 2
 };
 
-int EXT2_info(int fd, int flag);
+typedef struct {
+	int error;
+	char filesystem[5];
+	uint32_t inodes_count;
+	uint32_t blocks_count;
+	uint32_t reserved_blocks;
+	uint32_t free_blocks;
+	uint32_t free_inodes;
+	uint32_t blocks_per_group;
+	uint32_t frags_per_group;
+	uint32_t inodes_per_group;
+	uint32_t first_inode;
+	uint16_t inode_size;
+	uint32_t block_size;
+	uint32_t first_block;
+	char volume_name[17];
+	uint32_t last_mount;
+	uint32_t last_write;
+	uint32_t last_check;
+}ext_info;
+
+void EXT2_find(int fd, ext_info info, char *filename);
+uint32_t get_inode_size(int fd, ext_info info, int inode);
+ext_info EXT2_info(int fd, int flag);
+char * unixt_to_date(uint32_t time);
 
 #endif
